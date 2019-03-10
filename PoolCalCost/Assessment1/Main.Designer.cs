7536d8f63662d8e59b20118adb5998425cf657cc
Joubert Lacerda <46083794+JoubertLacerda@users.noreply.github.com>
2019-03-10 16:03:35 +1000

    Add files via upload

---
+++ PoolCalCost/Assessment1/Main.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Assessment1
+{
+    partial class PoolCal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PoolSizeAndCost = new System.Windows.Forms.Label();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAvgDepth = new System.Windows.Forms.TextBox();
+            this.lblm = new System.Windows.Forms.Label();
+            this.lblm5 = new System.Windows.Forms.Label();
+            this.lblm6 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblAvgTemp = new System.Windows.Forms.Label();
+            this.lblDollarsPerMonth = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSurfaceArea = new System.Windows.Forms.Label();
+            this.txtSurfaceArea = new System.Windows.Forms.TextBox();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.txtTableDollars = new System.Windows.Forms.TextBox();
+            this.txtTableTemp = new System.Windows.Forms.TextBox();
+            this.lblSqMeter = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PoolSizeAndCost
+            // 
+            this.PoolSizeAndCost.AutoSize = true;
+            this.PoolSizeAndCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PoolSizeAndCost.Location = new System.Drawing.Point(91, 25);
+            this.PoolSizeAndCost.Name = "PoolSizeAndCost";
+            this.PoolSizeAndCost.Size = new System.Drawing.Size(314, 39);
+            this.PoolSizeAndCost.TabIndex = 0;
+            this.PoolSizeAndCost.Text = "Pool Size And Cost";
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(75, 81);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(40, 13);
+            this.lblLength.TabIndex = 1;
+            this.lblLength.Text = "&Length";
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(75, 140);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(35, 13);
+            this.lblWidth.TabIndex = 2;
+            this.lblWidth.Text = "&Width";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(158, 74);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 3;
+            this.txtLength.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(158, 133);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 4;
+            this.txtWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Average &Depth";
+            // 
+            // txtAvgDepth
+            // 
+            this.txtAvgDepth.Location = new System.Drawing.Point(158, 187);
+            this.txtAvgDepth.Name = "txtAvgDepth";
+            this.txtAvgDepth.Size = new System.Drawing.Size(100, 20);
+            this.txtAvgDepth.TabIndex = 6;
+            this.txtAvgDepth.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblm
+            // 
+            this.lblm.AutoSize = true;
+            this.lblm.Location = new System.Drawing.Point(264, 74);
+            this.lblm.Name = "lblm";
+            this.lblm.Size = new System.Drawing.Size(15, 13);
+            this.lblm.TabIndex = 7;
+            this.lblm.Text = "m";
+            // 
+            // lblm5
+            // 
+            this.lblm5.AutoSize = true;
+            this.lblm5.Location = new System.Drawing.Point(264, 133);
+            this.lblm5.Name = "lblm5";
+            this.lblm5.Size = new System.Drawing.Size(15, 13);
+            this.lblm5.TabIndex = 8;
+            this.lblm5.Text = "m";
+            // 
+            // lblm6
+            // 
+            this.lblm6.AutoSize = true;
+            this.lblm6.Location = new System.Drawing.Point(264, 194);
+            this.lblm6.Name = "lblm6";
+            this.lblm6.Size = new System.Drawing.Size(15, 13);
+            this.lblm6.TabIndex = 9;
+            this.lblm6.Text = "m";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(158, 255);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblAvgTemp
+            // 
+            this.lblAvgTemp.AutoSize = true;
+            this.lblAvgTemp.Location = new System.Drawing.Point(653, 93);
+            this.lblAvgTemp.Name = "lblAvgTemp";
+            this.lblAvgTemp.Size = new System.Drawing.Size(59, 13);
+            this.lblAvgTemp.TabIndex = 11;
+            this.lblAvgTemp.Text = "AVG Temp";
+            // 
+            // lblDollarsPerMonth
+            // 
+            this.lblDollarsPerMonth.AutoSize = true;
+            this.lblDollarsPerMonth.Location = new System.Drawing.Point(817, 93);
+            this.lblDollarsPerMonth.Name = "lblDollarsPerMonth";
+            this.lblDollarsPerMonth.Size = new System.Drawing.Size(65, 13);
+            this.lblDollarsPerMonth.TabIndex = 12;
+            this.lblDollarsPerMonth.Text = "$ Per Month";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 385);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Volume Of Water";
+            // 
+            // lblSurfaceArea
+            // 
+            this.lblSurfaceArea.AutoSize = true;
+            this.lblSurfaceArea.Location = new System.Drawing.Point(64, 334);
+            this.lblSurfaceArea.Name = "lblSurfaceArea";
+            this.lblSurfaceArea.Size = new System.Drawing.Size(69, 13);
+            this.lblSurfaceArea.TabIndex = 14;
+            this.lblSurfaceArea.Text = "Surface Area";
+            // 
+            // txtSurfaceArea
+            // 
+            this.txtSurfaceArea.Location = new System.Drawing.Point(158, 327);
+            this.txtSurfaceArea.Name = "txtSurfaceArea";
+            this.txtSurfaceArea.ReadOnly = true;
+            this.txtSurfaceArea.Size = new System.Drawing.Size(100, 20);
+            this.txtSurfaceArea.TabIndex = 15;
+            this.txtSurfaceArea.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Location = new System.Drawing.Point(158, 378);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.ReadOnly = true;
+            this.txtVolume.Size = new System.Drawing.Size(100, 20);
+            this.txtVolume.TabIndex = 16;
+            this.txtVolume.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtTableDollars
+            // 
+            this.txtTableDollars.Location = new System.Drawing.Point(820, 126);
+            this.txtTableDollars.Multiline = true;
+            this.txtTableDollars.Name = "txtTableDollars";
+            this.txtTableDollars.ReadOnly = true;
+            this.txtTableDollars.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtTableDollars.Size = new System.Drawing.Size(100, 248);
+            this.txtTableDollars.TabIndex = 18;
+            this.txtTableDollars.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtTableTemp
+            // 
+            this.txtTableTemp.Location = new System.Drawing.Point(656, 126);
+            this.txtTableTemp.Multiline = true;
+            this.txtTableTemp.Name = "txtTableTemp";
+            this.txtTableTemp.ReadOnly = true;
+            this.txtTableTemp.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtTableTemp.Size = new System.Drawing.Size(100, 248);
+            this.txtTableTemp.TabIndex = 19;
+            this.txtTableTemp.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblSqMeter
+            // 
+            this.lblSqMeter.AutoSize = true;
+            this.lblSqMeter.Location = new System.Drawing.Point(287, 334);
+            this.lblSqMeter.Name = "lblSqMeter";
+            this.lblSqMeter.Size = new System.Drawing.Size(52, 13);
+            this.lblSqMeter.TabIndex = 20;
+            this.lblSqMeter.Text = "sq meters";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(287, 385);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "liters at 25C";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(653, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(267, 60);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Cost to heat pool to 25 degrees Celcius based on varying average daily temperture" +
+    "";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(981, 486);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 23;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(64, 433);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(0, 13);
+            this.lblCategory.TabIndex = 24;
+            // 
+            // PoolCal
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1106, 521);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblSqMeter);
+            this.Controls.Add(this.txtTableTemp);
+            this.Controls.Add(this.txtTableDollars);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.txtSurfaceArea);
+            this.Controls.Add(this.lblSurfaceArea);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDollarsPerMonth);
+            this.Controls.Add(this.lblAvgTemp);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblm6);
+            this.Controls.Add(this.lblm5);
+            this.Controls.Add(this.lblm);
+            this.Controls.Add(this.txtAvgDepth);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.PoolSizeAndCost);
+            this.Name = "PoolCal";
+            this.Text = " Pool Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PoolSizeAndCost;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAvgDepth;
+        private System.Windows.Forms.Label lblm;
+        private System.Windows.Forms.Label lblm5;
+        private System.Windows.Forms.Label lblm6;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblAvgTemp;
+        private System.Windows.Forms.Label lblDollarsPerMonth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSurfaceArea;
+        private System.Windows.Forms.TextBox txtSurfaceArea;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.TextBox txtTableDollars;
+        private System.Windows.Forms.TextBox txtTableTemp;
+        private System.Windows.Forms.Label lblSqMeter;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblCategory;
+    }
+}
+
---
+++ PoolCalCost/Assessment1/Main.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*Current BUGS : Program does not display values on first click (found on 5/10/2016)
+ * txtTableTemp and txtTableDollaris not going to next line (found on 5/25/2016)
+ */
+
+
+namespace Assessment1
+{
+    public partial class PoolCal : Form
+    {
+        const float AVGTEMPLOWER = 5;
+        const float AVGTEMPUPPER = 23;
+        const float AVGTEMPSTEP = 1.5F;
+        const long HEATINGADJUSTMENT = 32500;
+        const int TARGETTEMP = 25;
+        
+        const int LENGTHLOWER = 5;
+        const int LENGTHUPPER = 50;
+        
+        const int WIDTHLOWER = 2;
+        const int WIDTHUPPER = 20;
+
+        const int AVGDEPTHLOWER = 2;
+        const int AVGDEPTHUPPER = 4;
+
+        const long SMALLPOOLUPPER = 500000;
+        const long MEDIUMPOOLUPPER = 1500000;
+
+        float Length;
+        float Width;
+        float AvgDepth;
+        float Volume;
+        float SurfaceArea;
+       
+        public PoolCal()
+        {
+            InitializeComponent();
+        }
+        
+       
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            float Width = float.Parse(txtWidth.Text);            
+            float Length = float.Parse(txtLength.Text);            
+            float AvgDepth = float.Parse(txtAvgDepth.Text);
+           
+
+            // if Width is NOT valid, this will return the ValidWidth method.
+            if (!(ValidWidth(float.Parse(txtWidth.Text))))
+            {
+                return;
+            }
+             //if Length is NOT valid, this will return the ValidLength method.           
+            if (!(ValidLength(float.Parse(txtLength.Text))))
+            {
+                return;
+            }
+            //if Average Depth is NOT valid, this will return the ValidAvgDepth method.
+            if (!(ValidAvgDepth(float.Parse(txtAvgDepth.Text))))
+            {
+                return;
+            }
+
+            // call's methods to find out the Volume,Surface Area, and size of pool and displays them to their '.Text' boxes. 
+            CalVolume(Length,Width,AvgDepth);
+            CalSurfArea(Length, Width);
+            CalPoolSize();
+            CalHeating();
+            
+        }
+
+        //This will check if Length of pool is valid.
+        public bool ValidLength(float Length)
+        {
+            bool ValidLength = false;
+
+            //if user input is less then 5 or greater the 50 then this will display the message.
+            if (Length >= 5 && Length <= 50)
+            {               
+            }
+            else
+            {
+                MessageBox.Show("Length measeurement is invalid. Please enter a value between 5 and 50");
+            }
+            return true;
+        }
+        
+        //This will Check if Width of pool is valid.
+        public bool ValidWidth(float Width)
+        {
+            bool ValidWidth = false;
+            // if user input is less then 2 or greater the 20 then this will display the message.
+            if (Width >= 2 && Width <= 20)
+            {             
+            }
+            else
+            {
+                MessageBox.Show("Width measurement is invalid. Please enter a value between 2 and 20");
+            }
+            return true;
+        }
+
+        //This will Check if Average Depth is valid.
+        public bool ValidAvgDepth(float AvgDepth)
+        {
+            bool ValidAvgDepth = false;
+            // if user input is less then 2 or greater then 4 then this will display message.
+            if (AvgDepth >= 2 && AvgDepth <= 4)
+            {                
+            }
+            else
+            {
+                MessageBox.Show("Average Depth measurement is invalid. Please enter a value between 2 and 4");
+            }
+            return true;
+        }
+
+        //This will Calculate Surface Area of pool;
+        private float CalSurfArea(float Length, float Width)
+        {
+            txtSurfaceArea.Text = SurfaceArea.ToString();
+            
+            SurfaceArea = Length * Width;
+
+            return SurfaceArea;     
+        }
+
+        //This will Calculate Volume of water in pool.
+         private float CalVolume(float Length, float Width, float AvgDepth)
+        {
+            txtVolume.Text = Volume.ToString();
+            
+            Volume = Length * Width * AvgDepth * 1000;
+
+            return Volume;
+        }
+
+        // This will check the size of the pool to see if the pool is Small, Medium, or Large.
+        private double CalPoolSize()
+        {
+            float PoolSize =0;
+            lblCategory.Text = PoolSize.ToString();
+             
+            if (PoolSize <=SMALLPOOLUPPER)
+            {
+               lblCategory.Text = "Pool Size: Small";           
+            }
+            else if (PoolSize >SMALLPOOLUPPER && PoolSize < MEDIUMPOOLUPPER)
+            {
+                lblCategory.Text = "Pool Size: Medium";
+            }
+            else if (PoolSize >MEDIUMPOOLUPPER)
+            {
+                lblCategory.Text = "Pool Size: Large";
+            }
+            else { }
+            return PoolSize;
+                     
+        }
+
+        //this will calculate the Dollars-per-month of the pool.
+        private float CalHeating()
+        {
+            float heating = AVGTEMPLOWER;
+            float step = AVGTEMPSTEP;
+
+            while (heating <= 23)
+            {                    
+                txtTableTemp.Text += heating.ToString() + "\r\n";                
+                txtTableDollars.Text += DollarPerMonth(heating).ToString() + "\r\n";
+                heating += step;
+            }
+            return heating;
+        }
+
+        private float DollarPerMonth(float temperature)
+        {
+          float HeatingCost = 0;
+            
+            HeatingCost = (TARGETTEMP - temperature) * Volume / HEATINGADJUSTMENT;
+            
+                return Convert.ToInt32(HeatingCost); 
+        }
+
+        // this will make the EXIT button close the application.        
+        private void btnExit_Click(object sender, EventArgs e)
+        { 
+            Application.Exit();
+        }        
+    }
+}
